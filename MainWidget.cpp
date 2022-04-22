c07796d877b9691e0ec8338085dec4557b740a0c
Tobias Heinemann <85802951+kuerbisbeule33@users.noreply.github.com>
2022-04-22 21:59:03 +0200

    first try of angle conversion formula

--- MainWidget.cpp
+++ MainWidget.cpp
@@ -1,4 +1,42 @@
 #include "MainWidget.h"
+#include <QtMath>
+
+const double armLengthHorizontal = 1.0;
+const double armLengthVertical = 1.0;
+const double gripLength = 1.0;
+
+double lawOfCosinus(double a, double b, double c){
+    return qAcos((a*a + b*b - c*c) / (2*a*b));
+}
+
+Point angleToCoordinates(quint16 rotationAngle, quint16 tiltAngle, quint16 horizontalAngle){
+    //x nach rechts
+    //y nach oben
+    //z nach vorne
+    Point point;
+    double rotation = static_cast<double>(rotationAngle);
+    double tilt = static_cast<double>(tiltAngle);
+    double horizontal = static_cast<double>(horizontalAngle);
+    double ditanzXZ = qCos(tilt) * armLengthVertical + qCos(180-tilt-horizontal) * armLengthHorizontal + gripLength;
+    point.x = qCos(rotation) * ditanzXZ;
+    point.y = qSin(tilt) * armLengthVertical - qSin(180-tilt-horizontal) * armLengthHorizontal;
+    point.z = -qSin(rotation) * ditanzXZ;
+    return point;
+}
+
+void CoordinatesToAngle(Point point, quint16& rotationAngle, quint16& tiltAngle, quint16& horizontalAngle){
+    //2 teile der Koordinaten von https://appliedgo.net/roboticarm/
+    //x nach rechts
+    //y nach oben
+    //z nach vorne
+    rotationAngle = static_cast<quint16>(qAtan2(-point.z, point.x));
+    double distOnGround = qSqrt(point.x*point.x + point.z*point.z);
+    double dist2Dimensions = qSqrt(point.y*point.y + distOnGround*distOnGround);
+    quint16 helpAngle1 = qAtan2(point.y, distOnGround);
+    quint16 helpAngle2 = static_cast<quint16>(lawOfCosinus(dist2Dimensions, armLengthVertical, armLengthHorizontal));
+    tiltAngle = helpAngle1 + helpAngle2;
+    horizontalAngle = static_cast<quint16>(lawOfCosinus(armLengthVertical, armLengthHorizontal, dist2Dimensions));
+}
 
 MainWidget::MainWidget(QWidget *parent)
     : QWidget{parent}
@@ -6,8 +44,8 @@ MainWidget::MainWidget(QWidget *parent)
     slider1 = new QSlider(Qt::Orientation::Horizontal);
     slider1->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     angle1 = new QSpinBox;
-    left1 = new QPushButton;
-    right1 = new QPushButton;
+    left1 = new QPushButton(QIcon("://left.png"), "");
+    right1 = new QPushButton(QIcon("://right.png"), "");
     slider1HBox = new QHBoxLayout;
     slider1HBox->addWidget(slider1);
     slider1HBox->addWidget(angle1);
@@ -17,8 +55,8 @@ MainWidget::MainWidget(QWidget *parent)
     slider2 = new QSlider(Qt::Orientation::Horizontal);
     slider2->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     angle2 = new QSpinBox;
-    left2 = new QPushButton;
-    right2 = new QPushButton;
+    left2 = new QPushButton(QIcon("://left.png"), "");
+    right2 = new QPushButton(QIcon("://right.png"), "");
     slider2HBox = new QHBoxLayout;
     slider2HBox->addWidget(slider2);
     slider2HBox->addWidget(angle2);
@@ -28,8 +66,8 @@ MainWidget::MainWidget(QWidget *parent)
     slider3 = new QSlider(Qt::Orientation::Horizontal);
     slider3->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     angle3 = new QSpinBox;
-    left3 = new QPushButton;
-    right3 = new QPushButton;
+    left3 = new QPushButton(QIcon("://left.png"), "");
+    right3 = new QPushButton(QIcon("://right.png"), "");
     slider3HBox = new QHBoxLayout;
     slider3HBox->addWidget(slider3);
     slider3HBox->addWidget(angle3);
@@ -39,8 +77,8 @@ MainWidget::MainWidget(QWidget *parent)
     slider4 = new QSlider(Qt::Orientation::Horizontal);
     slider4->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     angle4 = new QSpinBox;
-    left4 = new QPushButton;
-    right4 = new QPushButton;
+    left4 = new QPushButton(QIcon("://left.png"), "");
+    right4 = new QPushButton(QIcon("://right.png"), "");
     slider4HBox = new QHBoxLayout;
     slider4HBox->addWidget(slider4);
     slider4HBox->addWidget(angle4);
--- MainWidget.h
+++ MainWidget.h
@@ -10,6 +10,21 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 
+extern const double armLengthHorizontal;
+extern const double armLengthVertical;
+extern const double gripLength;
+
+struct Point {
+    double x;
+    double y;
+    double z;
+};
+
+double lawOfCosinus(double a, double b, double c);
+Point angleToCoordinates(quint16 rotationAngle, quint16 tiltAngle, quint16 horizontalAngle);
+void CoordinatesToAngle(Point point, quint16& rotationAngle, quint16& tiltAngle, quint16& horizontalAngle);
+
+
 class MainWidget : public QWidget
 {
     Q_OBJECT
@@ -48,5 +63,6 @@ signals:
 
 
 };
+
 
 #endif // MAINWIDGET_H
